43df9c08749ba4bdcb81004b60c92373bbc747b7
lionel chamouilli <lionel.chamouilli@gmail.com>
2022-03-30 17:01:34 +0200

    Image asset in show

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 Character.destroy_all
 User.destroy_all
@@ -13,24 +14,85 @@ user1 = User.create!(first_name: "Camille", last_name: "Marquez", email: "test@t
 user2 = User.create!(first_name: "Rebecca", last_name: "Biberian", email: "test@test.fr", password: "123456", password_confirmation:"123456")
 user3 = User.create!(first_name: "Paul", last_name: "James", email: "test@test.org", password: "123456", password_confirmation:"123456")
 user4 = User.create!(first_name: "Lionel", last_name: "Chamouilli", email: "test@test.eu", password: "123456", password_confirmation:"123456")
+#------- Images------#
+puts "titi"
+# img_1 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636333/development/1r513jnil5348dud7p4zik4sq04l.png')
+img_2 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636810/development/zg8j7l5cgm57c92ykozeyu0nptc1.png')
+img_3 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636826/development/1tjjbvye8iq10p4ako5f4m8smic9.webp')
+img_4 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636844/development/scyvd3bgskjo04nkwkpwsn8n02nq.png')
+img_5 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636860/development/6twfnl39p55wz19irbs833emf5lb.png')
+img_6 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636875/development/1rzp8xpvoq3u3r1ow7ulfnmi2k6i.jpg')
+img_7 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636903/development/2o2p4f1p4njle0w0dw3ozxyaq3pj.png')
+img_8 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636916/development/8tn5zd4trv6px4btd9z42xccom2x.jpg')
+img_9 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636928/development/11ub5acq07fy8wvmhp0s7p0ihxf6.png')
+img_10 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636943/development/t27l6jw70ahffwrfvel7mygg4syz.png')
+img_11 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636963/development/rw7c4i2ts69beggjdknuann8q9cm.png')
+img_12 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636977/development/66h2vo9ecoo4ycq41x1z2r5j3d98.png')
+img_13 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648636989/development/ejpytuyoquc78czsxw9jr814rg2m.png')
+img_14 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648637005/development/b2fwarndd2pmej6r3rtm4f13nwq4.png')
+img_15 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648637025/development/rormde5iv03pfm468foge5o4ldk0.png')
+img_16 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648637039/development/o0yf8t9gvlb1ihk1mtjn9v67w4dn.png')
+img_17 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648637051/development/cf9r8sv1xd7qd1z9ktdb03qtfnjx.png')
+img_18 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648637115/development/b56nh97sdxhwh7rg84mv8nj7e3ye.png')
+img_19 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648637115/development/b56nh97sdxhwh7rg84mv8nj7e3ye.png')
+img_20 = URI.open('https://res.cloudinary.com/dawghi0op/image/upload/v1648637150/development/r5gdehis5z7s29a5livhoh268i85.jpg')
 
-Character.create!(nickname: 'Homer Simpson', average_rating: 0, description: 'Célébre habitant de Springfield', price_hour: 50, user: user1)
-Character.create!(nickname: 'Batman', average_rating: 0, description: 'Super-héros de Gotham City ', price_hour: 100, user: user1)
-Character.create!(nickname: 'Tintin', average_rating: 0, description: 'Célébre journaliste', price_hour: 25, user: user1)
-Character.create!(nickname: 'Kenny', average_rating: 0, description: 'Garçon de la petite ville de South-park', price_hour: 50, user: user1)
-Character.create!(nickname: 'Garfield', average_rating: 0, description: 'Le meilleur gros chat roux, flemmard, et fan de Lasagne', price_hour: 5, user: user1)
-Character.create!(nickname: 'Tank girl', average_rating: 0, description: 'Rebecca Buck parcourt une Australie post-apocalyptique en compagnie d’un kangourou mutant.', price_hour: 200, user: user2)
-Character.create!(nickname: 'Daria morgendorfer', average_rating: 0, description: 'une lycéenne avec un humour sarcastique', price_hour: 60, user: user2)
-Character.create!(nickname: 'Poison Ivy', average_rating: 0, description: 'Célébre méchante de Gotham City qui controle la nature', price_hour: 80, user: user2)
-Character.create!(nickname: 'Mr. Esclave', average_rating: 0, description: 'Sadomasochiste', price_hour: 2, user: user2)
-Character.create!(nickname: 'Catwoman', average_rating: 0, description: 'Voleuse célébre de Gotham City', price_hour: 500, user: user2)
-Character.create!(nickname: 'Chat de Cheshire', average_rating: 0, description: 'Chat malicieux du pays des merveilles', price_hour: 600, user: user3)
-Character.create!(nickname: 'Obi-wan Kenobi', average_rating: 0, description: 'Grand maitre Jedi', price_hour: 199, user: user3)
-Character.create!(nickname: 'Thor', average_rating: 0, description: 'Dieux du tonnerre, prince de Asgard et membre des Avengers', price_hour: 100, user: user3)
-Character.create!(nickname: 'Marty Mcfly', average_rating: 0, description: 'Meilleur amie du Doc, il voyage dans le temps grâce à la Delorean', price_hour: 999, user: user3)
-Character.create!(nickname: 'Dr Peter Venkman', average_rating: 0, description: 'Docteur et membres des célébres Ghostbusters', price_hour: 30, user: user3)
-Character.create!(nickname: 'Rick', average_rating: 0, description: 'Rick étant un inventeur extrêmement doué', price_hour: 80, user: user4)
-Character.create!(nickname: 'Hall', average_rating: 0, description: 'Le père le plus infantile qui soit', price_hour: 70, user: user4)
-Character.create!(nickname: 'Phoebe', average_rating: 0, description: 'Fille excentrique et attachante', price_hour: 70, user: user4)
-Character.create!(nickname: 'Charles Boyle', average_rating: 0, description: 'Officier peu charismatique de la brigade Brooklyn 99', price_hour: 80, user: user4)
-Character.create!(nickname: 'Michael', average_rating: 0, description: 'Patron incompétent', price_hour: 75, user: user4)
+character_1 = Character.create!(nickname: 'Homer Simpson', average_rating: 0, description: 'Célébre habitant de Springfield', price_hour: 50, user: user1)
+character_1.photo.attach(io: img_1, filename: "Omer Simpson", content_type: 'image/png')
+
+character_2 = Character.create!(nickname: 'Batman', average_rating: 0, description: 'Super-héros de Gotham City ', price_hour: 100, user: user1)
+character_2.photo.attach(io: img_2, filename: "Omer Simpson", content_type: 'image/png')
+
+character_3 = Character.create!(nickname: 'Tintin', average_rating: 0, description: 'Célébre journaliste', price_hour: 25, user: user1)
+character_3.photo.attach(io: img_3, filename: "Omer Simpson", content_type: 'image/webp')
+
+character_4 = Character.create!(nickname: 'Kenny', average_rating: 0, description: 'Garçon de la petite ville de South-park', price_hour: 50, user: user1)
+character_4.photo.attach(io: img_4, filename: "Omer Simpson", content_type: 'image/png')
+
+character_5 = Character.create!(nickname: 'Garfield', average_rating: 0, description: 'Le meilleur gros chat roux, flemmard, et fan de Lasagne', price_hour: 5, user: user1)
+character_5.photo.attach(io: img_5, filename: "Omer Simpson", content_type: 'image/png')
+puts "toto"
+character_6 = Character.create!(nickname: 'Tank girl', average_rating: 0, description: 'Rebecca Buck parcourt une Australie post-apocalyptique en compagnie d’un kangourou mutant.', price_hour: 200, user: user2)
+character_6.photo.attach(io: img_6, filename: "Omer Simpson", content_type: 'image/jpg')
+
+character_7 = Character.create!(nickname: 'Daria morgendorfer', average_rating: 0, description: 'une lycéenne avec un humour sarcastique', price_hour: 60, user: user2)
+character_7.photo.attach(io: img_7, filename: "Omer Simpson", content_type: 'image/png')
+
+character_8 = Character.create!(nickname: 'Poison Ivy', average_rating: 0, description: 'Célébre méchante de Gotham City qui controle la nature', price_hour: 80, user: user2)
+character_8.photo.attach(io: img_8, filename: "Omer Simpson", content_type: 'image/jpg')
+
+character_9 = Character.create!(nickname: 'Mr. Esclave', average_rating: 0, description: 'Sadomasochiste', price_hour: 2, user: user2)
+character_9.photo.attach(io: img_9, filename: "Omer Simpson", content_type: 'image/png')
+
+character_10 = Character.create!(nickname: 'Catwoman', average_rating: 0, description: 'Voleuse célébre de Gotham City', price_hour: 500, user: user2)
+character_10.photo.attach(io: img_10, filename: "Omer Simpson", content_type: 'image/png')
+
+character_11 = Character.create!(nickname: 'Chat de Cheshire', average_rating: 0, description: 'Chat malicieux du pays des merveilles', price_hour: 600, user: user3)
+character_11.photo.attach(io: img_11, filename: "Omer Simpson", content_type: 'image/png')
+
+character_12 = Character.create!(nickname: 'Obi-wan Kenobi', average_rating: 0, description: 'Grand maitre Jedi', price_hour: 199, user: user3)
+character_12.photo.attach(io: img_12, filename: "Omer Simpson", content_type: 'image/png')
+
+character_13 = Character.create!(nickname: 'Thor', average_rating: 0, description: 'Dieux du tonnerre, prince de Asgard et membre des Avengers', price_hour: 100, user: user3)
+character_13.photo.attach(io: img_13, filename: "Omer Simpson", content_type: 'image/png')
+
+character_14 = Character.create!(nickname: 'Marty Mcfly', average_rating: 0, description: 'Meilleur amie du Doc, il voyage dans le temps grâce à la Delorean', price_hour: 999, user: user3)
+character_14.photo.attach(io: img_14, filename: "Omer Simpson", content_type: 'image/png')
+
+character_15 = Character.create!(nickname: 'Dr Peter Venkman', average_rating: 0, description: 'Docteur et membres des célébres Ghostbusters', price_hour: 30, user: user3)
+character_15.photo.attach(io: img_15, filename: "Omer Simpson", content_type: 'image/png')
+puts "TOTO"
+character_16 = Character.create!(nickname: 'Rick', average_rating: 0, description: 'Rick étant un inventeur extrêmement doué', price_hour: 80, user: user4)
+character_16.photo.attach(io: img_16, filename: "Omer Simpson", content_type: 'image/png')
+
+character_17 = Character.create!(nickname: 'Hall', average_rating: 0, description: 'Le père le plus infantile qui soit', price_hour: 70, user: user4)
+character_17.photo.attach(io: img_17, filename: "Omer Simpson", content_type: 'image/png')
+
+character_18 = Character.create!(nickname: 'Phoebe', average_rating: 0, description: 'Fille excentrique et attachante', price_hour: 70, user: user4)
+character_18.photo.attach(io: img_18, filename: "Omer Simpson", content_type: 'image/png')
+
+character_19 = Character.create!(nickname: 'Charles Boyle', average_rating: 0, description: 'Officier peu charismatique de la brigade Brooklyn 99', price_hour: 80, user: user4)
+character_19.photo.attach(io: img_19, filename: "Omer Simpson", content_type: 'image/png')
+
+character_20 = Character.create!(nickname: 'Michael', average_rating: 0, description: 'Patron incompétent', price_hour: 75, user: user4)
+character_20.photo.attach(io: img_20, filename: "Omer Simpson", content_type: 'image/jpg')
